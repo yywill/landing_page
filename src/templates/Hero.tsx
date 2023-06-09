0b923df49e80d6acca282064790fecc579977854
William Yang <william.yang@bkyo.io>
2023-06-09 15:44:53 +0800

    update hero part

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -12,13 +12,13 @@ const Hero = () => (
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            <a>GitHub</a>
+          <Link href="https://www.btcstudy.org/">
+            <a>BTC Study</a>
           </Link>
         </li>
         <li>
-          <Link href="/">
-            <a>Sign in</a>
+          <Link href="https://mirror.xyz/williamdao.eth">
+            <a>William DAO</a>
           </Link>
         </li>
       </NavbarTwoColumns>
@@ -28,15 +28,15 @@ const Hero = () => (
       <HeroOneButton
         title={
           <>
-            {'The modern landing page for\n'}
-            <span className="text-primary-500">React developers</span>
+            {'The only way for\n'}
+            <span className="text-primary-500">Bitcoin Holder</span>
           </>
         }
-        description="The easiest way to build a React landing page in seconds."
+        description="The safe way to hold bitcoin in decades."
         button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
+          <Link href="https://william.yang.at.trustlessenterprise.com/">
             <a>
-              <Button xl>Download Your Free Theme</Button>
+              <Button xl>Consult For Details</Button>
             </a>
           </Link>
         }
