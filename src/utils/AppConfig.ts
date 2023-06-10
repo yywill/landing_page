978c234664ebd3118644201d12b4ba0ff0f622a0
yywill <1792088+yywill@users.noreply.github.com>
2023-06-10 09:39:01 +0000

    change social sharing description

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -2,6 +2,6 @@ export const AppConfig = {
   site_name: 'Trustless Enterprise',
   title: 'Do not trust, verify',
   description:
-    'The only way to hold Bitcoin forever. Do not SAVE your bitcoin, expense. ALL Custodial are ponzy shema.',
+    'The most secure way to hold Bitcoin long-term is through self-custody. Spending your Bitcoin supports the health of the network, whereas saving primarily in custodial accounts risks loss of funds if services shut down or accounts are hacked.',
   locale: 'en',
 };
