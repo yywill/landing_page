56ca682f0cbf1ec841632621c16faad19a741d9a
William Yang <william.yang@bkyo.io>
2023-06-16 22:55:47 +0800

    减少字体空间

--- src/feature/VerticalFeatureRow.tsx
+++ src/feature/VerticalFeatureRow.tsx
@@ -26,7 +26,7 @@ const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
     <div className={verticalFeatureClass}>
       <div className="w-full sm:w-1/2 text-center sm:px-6">
         <h3 className="text-3xl text-gray-900 font-semibold">{props.title}</h3>
-        <div className="mt-6 text-base leading-9">{props.description}</div>
+        <div className="mt-2 text-base leading-6">{props.description}</div>
       </div>
 
       <div className="w-full sm:w-1/2 p-6">
